b22c2ebaf0b7b02f5f51123b695c25508b5b78bc
Christine Spang <spang@mit.edu>
2009-06-17 12:17:44 +0300

    Move Config::GitLike::Cascaded doc to the end of its file.

--- lib/Config/GitLike/Cascaded.pm
+++ lib/Config/GitLike/Cascaded.pm
@@ -9,6 +9,24 @@ use File::Spec;
 
 extends 'Config::GitLike';
 
+sub load_dirs {
+    my $self = shift;
+    my $path = shift;
+    my($vol, $dirs, undef) = File::Spec->splitpath( $path, 1 );
+    my @dirs = File::Spec->splitdir( $dirs );
+    for my $i ( 1 .. $#dirs ) {
+        my $path = File::Spec->catpath( $vol, File::Spec->catdir(@dirs[0..$i]), $self->dir_file );
+        $self->load_file( $path ) if -e $path;
+    }
+}
+
+__PACKAGE__->meta->make_immutable;
+no Moose;
+
+1;
+
+__END__
+
 =head1 NAME
 
 Config::GitLike::Cascaded - git-like config file parsing with cascaded inheritance
@@ -40,26 +58,6 @@ directory and walking up to the current working directory. (No error is thrown
 if no config files are found.)
 
 Returns nothing of note.
-
-=cut
-
-sub load_dirs {
-    my $self = shift;
-    my $path = shift;
-    my($vol, $dirs, undef) = File::Spec->splitpath( $path, 1 );
-    my @dirs = File::Spec->splitdir( $dirs );
-    for my $i ( 1 .. $#dirs ) {
-        my $path = File::Spec->catpath( $vol, File::Spec->catdir(@dirs[0..$i]), $self->dir_file );
-        $self->load_file( $path ) if -e $path;
-    }
-}
-
-__PACKAGE__->meta->make_immutable;
-no Moose;
-
-1;
-
-__END__
 
 =head1 SEE ALSO
 

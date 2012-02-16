bestpractical/config-gitlike

commit dcdd01f23ebd1cf481097b91c535a10fc7e8d8f4

Do simple canonicalization prior to loading a file
This moves the ~ expansion somewhat more centrally, as well as helping
not load a file more than once if it is specified by both relative and
absolute paths.

lib/Config/GitLike.pm
@@ -121,7 +121,7 @@ sub load_global {
 sub user_file {
     my $self = shift;
     return
-        File::Spec->catfile( $ENV{'HOME'}, "." . $self->confname );
+        File::Spec->catfile( "~", "." . $self->confname );
 }
 
 sub load_user {
@@ -147,6 +147,10 @@ sub _read_config {
 sub load_file {
     my $self = shift;
     my ($filename) = @_;
+
+    # Do some canonicalization
+    $filename =~ s/~/$ENV{'HOME'}/g;
+    $filename = File::Spec->rel2abs($filename);
 
     return $self->data if grep {$_ eq $filename} @{$self->config_files};
 

commit 5f4a8c8ceb620f4cbcd690cfc60b29c6dbd5a67c

Fix loading of user_file, broken in dcdd01f, due to unexpanded ~
dcdd01f broke loading of ->user_file, by moving ~ expansion into
->load_file -- but ->load_user checked the file-ness of the raw value of
->user_file, which included the literal ~.  This means that user files
were generally only loaded because the target directory was beneath the
user's home directory -- and Config::GitLike::Git _never_ loaded the
user's ~/.gitconfig !

Instead of having ->load_user and ->load_global check the validity of
their arguments explicitly, rely on ->load_file's ability to tolerate
being called on non-existant files.  Push the previous -f check down
into ->_read_config to handle odd corner cases, such as the filename
being to a directory, which open() tolerates on some systems.

Reported-by: Chia-liang Kao <clkao@clkao.org>

lib/Config/GitLike.pm
@@ -114,7 +114,6 @@ sub global_file {
 
 sub load_global {
     my $self = shift;
-    return unless -f $self->global_file;
     return $self->load_file( $self->global_file );
 }
 
@@ -126,7 +125,6 @@ sub user_file {
 
 sub load_user {
     my $self = shift;
-    return unless -f $self->user_file;
     return $self->load_file( $self->user_file );
 }
 
@@ -135,6 +133,7 @@ sub _read_config {
     my $self = shift;
     my $filename = shift;
 
+    return unless -f $filename and -r $filename;
     open(my $fh, '<', $filename) or return;
 
     my $c = do {local $/; <$fh>};